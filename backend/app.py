cc8ffcd9f3a337e76246ddc9d5584643bfc4c2e3
Subhalakshmi <subhalakshmi@gmail.com>
2026-05-12 12:42:42 +0530

    Fixed Render deployment

--- backend/app.py
+++ backend/app.py
@@ -95,5 +95,6 @@ def delete_task(task_id):
 
 # ---------------- RUN SERVER ----------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    with app.app_context():
+    db.create_all()
     app.run(host="0.0.0.0", port=port)
